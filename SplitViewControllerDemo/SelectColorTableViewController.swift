2bce7a4c1c3bacc37785d73c443088d2718f8e37
Joel Klabo <joelklabo@gmail.com>
2016-11-19 16:05:07 -0800

    Updating to Swift 3
    * Also increased deployment target to 8.4

--- SplitViewControllerDemo/SelectColorTableViewController.swift
+++ SplitViewControllerDemo/SelectColorTableViewController.swift
@@ -15,20 +15,20 @@ struct Color {
 
 class SelectColorTableViewController: UITableViewController {
 
-    private let colorCellIdentifier = "colorCell"
+    fileprivate let colorCellIdentifier = "colorCell"
     
     var collapseDetailViewController: Bool  = true
     
-    private let colors = [
-        Color(displayName: "Green", color: UIColor.greenColor()),
-        Color(displayName: "Blue", color: UIColor.blueColor()),
-        Color(displayName: "Yellow", color: UIColor.yellowColor()),
-        Color(displayName: "Purple", color: UIColor.purpleColor()),
-        Color(displayName: "Orange", color: UIColor.orangeColor()),
-        Color(displayName: "Magenta", color: UIColor.magentaColor()),
-        Color(displayName: "Brown", color: UIColor.brownColor()),
-        Color(displayName: "Cyan", color: UIColor.cyanColor()),
-        Color(displayName: "Red", color: UIColor.redColor())
+    fileprivate let colors = [
+        Color(displayName: "Green", color: UIColor.green),
+        Color(displayName: "Blue", color: UIColor.blue),
+        Color(displayName: "Yellow", color: UIColor.yellow),
+        Color(displayName: "Purple", color: UIColor.purple),
+        Color(displayName: "Orange", color: UIColor.orange),
+        Color(displayName: "Magenta", color: UIColor.magenta),
+        Color(displayName: "Brown", color: UIColor.brown),
+        Color(displayName: "Cyan", color: UIColor.cyan),
+        Color(displayName: "Red", color: UIColor.red)
     ]
     
     override func viewDidLoad() {
@@ -37,7 +37,7 @@ class SelectColorTableViewController: UITableViewController {
         
     }
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         // identifier if required, if you have more then one segue
         // it could be set via IB, i did ...
@@ -47,7 +47,7 @@ class SelectColorTableViewController: UITableViewController {
             var colorViewController: ColorViewController!
             
             // with help of adaptive segue we can support all devices
-            if let colorNavigationController = segue.destinationViewController as? UINavigationController {
+            if let colorNavigationController = segue.destination as? UINavigationController {
 
                 // works on devices where UISplitViewController is implemented
                 colorViewController = colorNavigationController.topViewController as! ColorViewController
@@ -56,11 +56,11 @@ class SelectColorTableViewController: UITableViewController {
             } else {
 
                 // works for iPhone on ios7, where UISplitViewController is not implemented
-                colorViewController = segue.destinationViewController as! ColorViewController
+                colorViewController = segue.destination as! ColorViewController
             }
             // this is common part, where one can configure detail view
             // segue provides a new instance of detail view everytime
-            if let selectedRowIndexPath = tableView.indexPathForSelectedRow() {
+            if let selectedRowIndexPath = tableView.indexPathForSelectedRow {
                 let color = colors[selectedRowIndexPath.row]
                 colorViewController.color = color
             }
@@ -69,14 +69,13 @@ class SelectColorTableViewController: UITableViewController {
     
     // MARK: - Table view data source
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return colors.count
     }
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCellWithIdentifier(colorCellIdentifier) as! UITableViewCell
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: colorCellIdentifier, for: indexPath) as UITableViewCell        
         let color = colors[indexPath.row]
         cell.textLabel?.text = color.displayName
         
@@ -85,7 +84,7 @@ class SelectColorTableViewController: UITableViewController {
     
     // MARK: Table View Delegate
 
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         collapseDetailViewController = false
     }
 
--- SplitViewControllerDemo/SplitViewSupport.swift
+++ SplitViewControllerDemo/SplitViewSupport.swift
@@ -18,8 +18,8 @@ extension UISplitViewController: UISplitViewControllerDelegate {
     
     var backBarButtonItem: UIBarButtonItem? {
         get {
-            if respondsToSelector(Selector("displayModeButtonItem")) == true {
-                let button: UIBarButtonItem = displayModeButtonItem()
+            if responds(to: #selector(getter: UISplitViewController.displayModeButtonItem)) == true {
+                let button: UIBarButtonItem = displayModeButtonItem
                 return button
             } else {
                 return ios7Support.modeButtonItem
@@ -36,28 +36,28 @@ extension UISplitViewController: UISplitViewControllerDelegate {
         return backBarButtonItem
     }
     
-    public func splitViewController(svc: UISplitViewController, willHideViewController aViewController: UIViewController, withBarButtonItem barButtonItem: UIBarButtonItem, forPopoverController pc: UIPopoverController) {
-        if (!svc.respondsToSelector(Selector("displayModeButtonItem"))) {
+    public func splitViewController(_ svc: UISplitViewController, willHide aViewController: UIViewController, with barButtonItem: UIBarButtonItem, for pc: UIPopoverController) {
+        if (!svc.responds(to: #selector(getter: UISplitViewController.displayModeButtonItem))) {
             if let detailView = svc.viewControllers[svc.viewControllers.count-1] as? UINavigationController {
                 svc.backBarButtonItem = barButtonItem
-                detailView.topViewController.navigationItem.leftBarButtonItem = barButtonItem
+                detailView.topViewController?.navigationItem.leftBarButtonItem = barButtonItem
             }
         }
     }
     
     
-    public func splitViewController(svc: UISplitViewController, willShowViewController aViewController: UIViewController, invalidatingBarButtonItem barButtonItem: UIBarButtonItem) {
-        if (!svc.respondsToSelector(Selector("displayModeButtonItem"))) {
+    public func splitViewController(_ svc: UISplitViewController, willShow aViewController: UIViewController, invalidating barButtonItem: UIBarButtonItem) {
+        if (!svc.responds(to: #selector(getter: UISplitViewController.displayModeButtonItem))) {
             if let detailView = svc.viewControllers[svc.viewControllers.count-1] as? UINavigationController {
                 svc.backBarButtonItem = nil
-                detailView.topViewController.navigationItem.leftBarButtonItem = nil
+                detailView.topViewController?.navigationItem.leftBarButtonItem = nil
             }
         }
     }
     
     // MARK: - user defined imlementation of UISplitViewControllerDelegate
     
-    public func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController: UIViewController!, ontoPrimaryViewController primaryViewController: UIViewController!) -> Bool {
+    public func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
         if let navController = primaryViewController as? UINavigationController {
             if let controller = navController.topViewController as? SelectColorTableViewController {
                 return controller.collapseDetailViewController
